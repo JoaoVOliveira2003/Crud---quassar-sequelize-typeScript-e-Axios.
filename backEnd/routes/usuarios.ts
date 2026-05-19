a96a4dee181eb407349df1c08730928e111e8fe3
joaoDeOliveiraPro <joao.oliveira@prodigitalhr.com.br>
2026-05-19 16:32:13 -0300

    teste push

--- backEnd/routes/usuarios.ts
+++ backEnd/routes/usuarios.ts
@@ -9,15 +9,14 @@ routerUsuarios.get('/usuariosIdNome', async (req,res)=> await usuarioController.
 routerUsuarios.get("/filtroUsuarios/", async (req, res) => await usuarioController.getUsuariosFiltrado(req, res));
 // routerUsuarios.use(validarBody) 
 routerUsuarios.post("/criarUsuario/", 
-   // validarTokenNaoObrigatorioMiddleware,
-   async (req, res) => await usuarioController.gravarUsuario(req, res));
+// validarTokenNaoObrigatorioMiddleware,
+async (req, res) => await usuarioController.gravarUsuario(req, res));
+
 // routerUsuarios.use(validarTokenObrigatorioMiddleware)
 routerUsuarios.get("", async (req, res) => await usuarioController.getUsuarios(res));
 routerUsuarios.put('/atualizarUsuario/:id',async(req,res)=> await usuarioController.atualizarUsuario(req,res));
 routerUsuarios.delete("/deletarUsuario/:id",async (req,res)=> await usuarioController.deletarUsuario(req,res));
 routerUsuarios.get('/perfilCookie', async (req,res)=> await usuarioController.perfilCookie(req,res));
-
 routerUsuarios.get('/countTiposUsuario', async (req,res)=> await usuarioController.getCountTipoUsuario(res))
-
 
 export default routerUsuarios;
